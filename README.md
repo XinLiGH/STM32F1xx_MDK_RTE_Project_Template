1b591ec35496529b126cc095d3164239d3e84c6c
XinLi <XinLiYX@Gmail.com>
2019-05-12 21:39:56 +0800

    添加文件 :yum:

---
+++ README.md
@@ -0,0 +1,9 @@
+# STM32F1xx_MDK_RTE_Project_Template
+
+STM32F1xx MDK RTE 项目模板。
+
+## 开发环境
+
+* IDE：Keil uVision5
+* 编译器：ARMCC V5.06
+* 操作系统：Windows 10 专业版
---
+++ STM32F1xx_MDK_RTE_RTOS2_Project_Template/RTE/_Debug/RTE_Components.h
@@ -0,0 +1,26 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32F1xx_MDK_RTE_RTOS2_Project_Template' 
+ * Target:  'Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+#define RTE_DEVICE_STDPERIPH_FLASH
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_RCC
+
+#endif /* RTE_COMPONENTS_H */
